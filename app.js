ac073bfc36962d280194756d96f8d856bb4d72c8
Roman Pelikh <pelikh.roman@gmail.com>
2019-03-23 17:07:21 -0300

    finished topics added faq

---
+++ app.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", () => {
+  let acc = document.getElementsByClassName("accordion");
+
+  for (let i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+
+      let panel = this.nextElementSibling;
+      if (panel.style.maxHeight) {
+        panel.style.maxHeight = null;
+      } else {
+        panel.style.maxHeight = panel.scrollHeight + "px";
+      }
+    });
+  }
+});
